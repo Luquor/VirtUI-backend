3f9582cd45164ea74ae1272e5eddf79070b752e3
Lucas Signorelli <signorellilucas7@gmail.com>
2024-01-09 11:14:44 +0100

    modify: handling des POST en structure

--- main.go
+++ main.go
@@ -6,9 +6,20 @@ import (
 	"net/http"
 )
 
+type Container struct {
+	Header string
+	Status string
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("static/stylesheets"))
 	http.Handle("/static/stylesheets/", http.StripPrefix("/static/stylesheets/", fs))
+
+	array := []Container{
+		Container{Header: "Portainer", Status: "Stoppé"},
+		Container{Header: "Grafana", Status: "Démarré"},
+		Container{Header: "Nginx Proxy Manager", Status: "Stoppé"},
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("templates/index.html")
@@ -16,7 +27,17 @@ func main() {
 			log.Fatal(err)
 		}
 
-		err = tmpl.Execute(w, nil)
+		if r.Method == "POST" && r.URL.Path == "/" {
+			r.ParseForm()
+			header := "Nouveaux containers"
+			status := "Démarré"
+			container := Container{Header: header, Status: status}
+			array = append(array, container)
+		}
+
+		log.Print(array)
+
+		err = tmpl.Execute(w, array)
 		if err != nil {
 			log.Fatal(err)
 		}
