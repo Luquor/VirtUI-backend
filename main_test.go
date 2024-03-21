813f8908e24897f12d8e1d782cf3e1c44f9521e5
DIDIer5454 <menene12.akoo@gmail.com>
2024-03-21 09:01:19 +0100

    delete

--- main_test.go
+++ main_test.go
@@ -5,21 +5,19 @@ import (
 	"os/exec"
 	"testing"
 	"virtui/models"
-	"strings"
 )
 
 // TestCreationContainer
 // for a valid return value.
 func TestCreationContainer(t *testing.T) {
-	cmd1 :=  exec.Command("sh","-c",`lxc image list | grep -oP '^\| [^ALIAS|]*\s\| (\w*)' | sed 's/|.*| //'`)
-	recupFingerPrint, err := cmd1.Output()
-	fmt.Println("fingerprint", cmd1, "fin")
-	name := "server"
-	models.CreateContainer(name, strings.TrimSuffix(string(recupFingerPrint),"\n"))
-	cmd := exec.Command("lxc", "query", "--request", "GET", "/1.0/instances/"+name)
-	instances, err := cmd.Output()
-	fmt.Println("cmd", cmd, "fin")
-	fmt.Println("instances,err", err, string(instances), "fin")
+	//cmd1 := exec.Command("sh", "-c", `lxc image list | grep -oP '^\| [^ALIAS|]*\s\| (\w*)' | sed 's/|.*| //'`)
+	//recupFingerPrint, err := cmd1.Output()
+	//name := "server"
+	//models.CreateContainer(name, strings.TrimSuffix(string(recupFingerPrint), "\n"))
+	//cmd := exec.Command("lxc", "query", "--request", "GET", "/1.0/instances/"+name)
+	//instances, err := cmd.Output()
+	//fmt.Println("cmd", cmd, "fin")
+	//fmt.Println("instances,err", err, string(instances), "fin")
 	//assert.Nil(t, err)
 	//assert.NotNil(t, instances)
 	//sudo lxc image copy images:f01555e462c4 didier:
@@ -38,15 +36,15 @@ func TestGetContainer(t *testing.T) {
 	//assert.NotNil(t, instances)
 }
 func TestSuppressionContainer(t *testing.T) {
-	//name := "server"
+	name := "server"
 	//recupFingerPrint, err := exec.Command("lxc", "image", "list", "|", "grep", "-oP", `^\| [^ALIAS|]*\s\| (\w*)`, "|", " sed ", `s/|.*| //`).Output()
 	//models.CreateContainer(name, string(recupFingerPrint))
-	//models.DeleteContainerWithName(name)
+	models.DeleteContainerWithName(name)
 	//supprimer := models.GetContainerWithName(name).Metadata
 	//fmt.Println("apres suppressions du conteneur:" + name)
-	//cmd := exec.Command("lxc", "query", "--request", "GET", "/1.0/instances/"+name)
-	//instances, err := cmd.Output()
-	//fmt.Println(err, instances)
+	cmd := exec.Command("lxc", "query", "--request", "GET", "/1.0/instances/"+name)
+	instances, err := cmd.Output()
+	fmt.Println(err, instances)
 	//if assert.NotNil(t, err) {
 	//var tab_byte []byte
 	//assert.Equal(t, string(tab_byte), string(instances))
